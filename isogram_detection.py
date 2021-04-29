fdb21dfb47138ae9b16502f2106afa0d5fc88086
Gosia <83029986+Gosia04@users.noreply.github.com>
2021-04-29 20:15:25 +0200

    Update isogram_detection.py

--- isogram_detection.py
+++ isogram_detection.py
@@ -1,9 +1,3 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[69]:
-
-
 def is_isogram(string):
     if type(string) == str and string.isalpha():
         s = string.lower()
@@ -14,16 +8,5 @@ def is_isogram(string):
     else:
         return print('Entered text is either empty, not a string or contains numbers')
 
-
-# In[71]:
-
-
 string = input("Enter string to check if it's an isogram: ")
 is_isogram(string)
-
-
-# In[ ]:
-
-
-
-
